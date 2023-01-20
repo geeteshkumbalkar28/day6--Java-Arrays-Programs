c675c35d9d7aff616d416f252e4c7e6dabbb6d96
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-01-21 00:57:39 +0530

    Added : convert char Array to String

---
+++ ArrayToString.java
@@ -0,0 +1,11 @@
+//Java Program to convert char Array to String
+
+public class ArrayToString
+{
+	public static void main(String arg[])
+	{
+		char arr[] = {'a','b','x','d','e'};
+		String str = String.valueOf(arr);
+		System.out.println("Convert array to string :"+str);
+	}
+}
